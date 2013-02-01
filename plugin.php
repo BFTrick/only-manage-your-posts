f58db9a0ecaa63d19236e56f2b4d053ab74a2e97
Patrick Rauland <bftrick@gmail.com>
2013-02-01 10:56:08 -0600

    adding comments & formatting

--- plugin.php
+++ plugin.php
@@ -3,15 +3,26 @@
 Plugin Name: Only Manage Your Posts
 Version: 0.1
 Plugin URI: http://code.mincus.com/41/manage-your-posts-only-in-wordpress/
-Description: Makes it so normal users can see only their posts and drafts from the manage posts screen.  Great for multi-user blogs where you want users to only see posts that they have created.  Wordpress already makes it so they cannot edit the posts, but still allows them to see the titles.  This can get annoying to find your posts mixed in with thousands of others.
+Description: Makes it so normal users can see only their posts and drafts from the manage posts screen.  Great for multi-user blogs where you want users to only see posts that they have created.
 Author: Patrick Rauland
-Author URI: http://www.patrickrauland..com
+Author URI: http://www.patrickrauland.com/
 */
 
 
-function omyp_parse_query_useronly( $wp_query ) {
-    if ( strpos( $_SERVER[ 'REQUEST_URI' ], '/wp-admin/edit.php' ) !== false ) {
-        if ( !current_user_can( 'level_10' ) ) {
+/**
+ * omyp_parse_query_useronly parse the query to only show one author
+ * @since 	0.1
+ * @param 	object $wp_query 
+ */
+function omyp_parse_query_useronly( $wp_query ) 
+{
+	// first check if we're on the right page
+    if ( strpos( $_SERVER[ 'REQUEST_URI' ], '/wp-admin/edit.php' ) !== false ) 
+    {
+    	// check to make sure the user doesn't have administrative capabilities
+        if ( !current_user_can( 'level_10' ) ) 
+        {
+        	// if the user doesn't have administrative queries then limit the query to just that user
             global $current_user;
             $wp_query->set( 'author', $current_user->id );
         }
